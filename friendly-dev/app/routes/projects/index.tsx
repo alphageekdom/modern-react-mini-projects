fb5653b692c2b3fcd25eff7278b6d828018beb6a
Luis <alphageekdom@gmail.com>
2025-11-02 11:58:20 -0800

    Added routes and pages

---
+++ friendly-dev/app/routes/projects/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsPage = () => {
+  return (
+    <section>
+      <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
+    </section>
+  );
+};
+
+export default ProjectsPage;
